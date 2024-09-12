c1741f78fcef13e7c81e9ce4d120dd0ffa3eff50
RunawayUI <tosha.grafkof@yandex.ru>
2024-09-12 21:43:10 +0300

    added ads search

--- src/pages/Advertisements.jsx
+++ src/pages/Advertisements.jsx
@@ -6,12 +6,12 @@ const Advertisements = () => {
   const [ads, setAds] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchAds = async () => {
       try {
         const response = await axios.get('http://localhost:5000/advertisements');
-        console.log(response.data);
         setAds(response.data);
         setLoading(false);
       } catch (error) {
@@ -24,23 +24,35 @@ const Advertisements = () => {
     fetchAds();
   }, []);
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const searchedAds = ads.filter(ad => 
+    ad.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   if (loading) return <div>Загрузка...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <div>
       <h1>Объявления</h1>
+      <input type="text" placeholder="Найти" onChange={handleSearch} value={search}/>
       <div className="ads-list">
-        {ads.map(ad => (
-          <div key={ad.id} className="ad">
-            <h2><Link to={`/advertisements/${ad.id}`}>{ad.name}</Link></h2>
-            {ad.description && <p>{ad.description}</p>}
-            <p>Цена: {ad.price} руб.</p>
-            <p>Просмотры: {ad.views}</p>
-            <p>Лайки: {ad.likes}</p>
-            <p>Дата создания: {new Date(ad.createdAt).toLocaleDateString()}</p>
-          </div>
-        ))}
+      {searchedAds.length > 0 ? (
+          searchedAds.map(ad => (
+            <div key={ad.id} className="ad">
+              <h2>{ad.name}</h2>
+              <p>Цена: {ad.price} руб.</p>
+              <p>Просмотры: {ad.views}</p>
+              <p>Лайки: {ad.likes}</p>
+              <Link to={`/advertisements/${ad.id}`}>Подробнее</Link>
+            </div>
+          ))
+        ) : (
+          <p>Объявления не найдены</p>
+        )}
       </div>
     </div>
   );
